a3188279c8097162444eae295b52c286c202bdbb
red812 <redbsvent@gmail.com>
2023-01-24 19:23:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+let count = 0;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+function increment() {
+  console.clear();
+  console.log("called");
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countstr = count + "-";
+  saveEl.textContent += countstr;
+}
+
+function reset() {
+  count = 0;
+  countEl.innerText = count;
+  saveEl.textContent = "Previous Entries: ";
+}
